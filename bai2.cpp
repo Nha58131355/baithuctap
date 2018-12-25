572c04f82021797a4b5e82cae40456e8bb12ac2d
Nha58131355 <ctvngsky@gmail.com>
2018-12-25 15:42:31 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -104,7 +104,7 @@ void STTSangNgay(NgayThang &b, int &n)
 void Cong(NgayThang a, int &x)
 {
 	
-	cout << "nhap so tu nhien duong: ";
+	cout << "\nnhap so cong them: ";
 	cin >> x;
 	
 	int n = STTday(a) + x;
@@ -119,36 +119,65 @@ void Cong(NgayThang a, int &x)
 		{
 			if(KTyear(b) == 0)
 				n++;
-			b.year++; // xong nam
+			b.year++; 
 		}
 	if(n == 365)
 	{
-		b.year++; // xong nam
-		b.day = 1; // xong ngay
-		b.month = 1; // xong thang
+		b.year++; 
+		b.day = 1; 
+		b.month = 1; 
 	}
 	else
 		if(n < 365)
-			STTSangNgay(b, n); // xong ngay va thang
+			STTSangNgay(b, n); 
 }
-/*NgayThang CongNgayThang(NgayThang n, int x)
+
+void Tru(NgayThang a, int &x)
 {
-	int kq = x + STTday(n);
-	int nam = n.year;
-	if(kq <= 365) return ngaytuSTT(kq, nam)
-	else
-		if(KTyear(n) == 1)
-			if(kq == 366) 
-				return ngaytuSTT(kq, nam)
-			else 
-				return ngaytuSTT(kq - 366, nam + 1 );
-		else 
-			return ngaytuSTT(kq - 365, nam +1)	
-}
-void add(NgayThang date, int x)
-{
+	cout << "nhap so ngay tru di: ";
+	cin >> x;
 	
-}*/
+	NgayThang b;
+	b.year = a.year;
+	b.month = 1;
+	b.day = 0;
+	
+	while(x >= 365)
+	{
+		if(KTyear(b) == 0)
+		{
+			b.year--;
+			x -= 365;
+		}
+		else
+		{
+			b.year--;
+			x -= 366;
+		}
+	}
+	
+	int stt = STTday(b);
+	
+	b.month = 1;
+	b.day = 0;
+	
+	if(stt > x)
+	{
+		stt -= x;
+		STTSangNgay(b, stt);
+	}
+	else
+	{
+		b.year--;
+		if(STTday(b) == true)
+			stt += 366;
+		else
+			stt += 365;
+		stt -= x;
+		STTSangNgay(b, stt);
+	}
+}
+
 int main()
 {
 	struct NgayThang date;
@@ -160,6 +189,8 @@ int main()
 	cout << "\nSTT ngay trong nam la: " << STTday(date);
 	int c;
 	Cong(date,c);
+	int t;
+	Tru(date,t);
 	
 }
 
