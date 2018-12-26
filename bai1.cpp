2fc36045b21916a1c26d3f6c5db1e1f7fec95f3f
Nha58131355 <ctvngsky@gmail.com>
2018-12-26 13:57:59 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -9,7 +9,7 @@ typedef struct
 	char ten[20];
 	char sdt[11];
 	char email[20],diachi[50];
-	char gtinh[3];
+	char gtinh[4];
 }Contact;
 
 //them moi contact vao dl
@@ -18,15 +18,15 @@ typedef struct
 void ThemMoi(Contact c)
 {
 	cout << "nhap ten: ";
-	cin.getline(c.ten, 20);
-	cout << "nhap sdt: ";
+	cin.getline(c.ten,20);
+	cout << "nhap so dien thoai: ";
 	cin.getline(c.sdt,11);
 	cout << "nhap email: ";
-	cin.getline(c.email,20);
+   	cin.getline(c.email,20);
 	cout << "nhap dia chi: ";
 	cin.getline(c.diachi,50);
 	cout << "nhap gioi tinh(Nam/Nu): ";
-	cin.getline(c.gtinh,3);
+    cin.getline(c.gtinh,4);	
 }
 
 void Nhapdanhba(ifstream &filein,Contact &c)
@@ -73,8 +73,7 @@ void xuatContact(Contact c)
 
 }
 
-
-int main()
+void LietKe()
 {
 	ifstream filein;
 	filein.open("D:/baithuctap/danhba.txt",ios_base::in);
@@ -85,4 +84,37 @@ int main()
 	{cout<<"===========================STT: "<<i+1<<"==========================="<<endl;
 		xuatContact(dsdanhba[i]);
 	}
+}
+
+void ThemDB()
+{
+	ifstream filein;
+	filein.open("D:/baithuctap/danhba.txt",ios_base::in);
+	vector<Contact> dsdanhba;
+	Nhapdanhba2(filein,dsdanhba);
+	filein.close();
+	cout << "\nthem danh ba moi:" << endl;
+	Contact tam;
+	ThemMoi(tam);
+	dsdanhba.push_back(tam);
+	ofstream outfile;
+	   outfile.open("D:/baithuctap/danhba.txt");
+	for(int i=0;i<dsdanhba.size();i++)
+	{
+		if(i==0) 
+			outfile<<dsdanhba[i].ten<<endl;
+		else
+			outfile<<endl<<dsdanhba[i].ten<<endl;
+			outfile<<dsdanhba[i].sdt<<endl;
+			outfile<<dsdanhba[i].email<<endl;
+			outfile<<dsdanhba[i].diachi<<endl;
+			outfile<<dsdanhba[i].gtinh;
+	}
+	filein.close();
+}
+
+int main()
+{
+	LietKe();
+	ThemDB();
 }
