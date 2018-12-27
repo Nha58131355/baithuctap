8d95c21f2029f5b5e828a75ab678cc19aae79ba4
Nha58131355 <ctvngsky@gmail.com>
2018-12-27 10:52:23 +0700

    Update bai3.cpp

--- bai3.cpp
+++ bai3.cpp
@@ -11,7 +11,7 @@ typedef struct
 {
 	string cauhoi;
 	string A,B,C;
-	char da;	
+	string da;	
 }CauHoi;
 
 typedef struct 
@@ -22,22 +22,49 @@ typedef struct
 
 vector<CauHoi> dscau;
 
+bool KiemTra (string A,string B)
+{
+	if(A.compare(B)==0)//ham so sanh chuoi string
+	 return true;
+	else
+		return false; 
+}
+
+void luudiem(string ten, int diem)
+{	
+	fstream f;
+	f.open("luudiem.txt", ios::out | ios::app);
+	f << ten << "\t\t" << diem << endl;
+	f.close();
+}
+
 void DocDe()
 {
 	ifstream f("de1.txt");
-	int sl;
-	string s;
+	int sl,diem=0;
+	string s,da,ten;
+	CauHoi bd1;
 	getline(f,s);
 	sl=atoi(s.c_str()); //doi string sang int
 	for(int i=0; i<sl; i++)
 	{
-		getline(f,s); dscau[i].cauhoi; cout <<s<< endl;
-		getline(f,s); dscau[i].A; cout << s << endl;
-		getline(f,s); dscau[i].B; cout << s << endl;
-		getline(f,s); dscau[i].C; cout << s << endl;
-		getline(f,s); dscau[i].da;
-		
+		getline(f,s); bd1.cauhoi=s; cout <<s<< endl;
+		getline(f,s); bd1.A=s; cout << s << endl;
+		getline(f,s); bd1.B=s; cout << s << endl;
+		getline(f,s); bd1.C=s; cout << s << endl;
+		getline(f,s); bd1.da=s;
+		cout << "dap an: ";
+		getline(cin,da);
+		if(KiemTra(bd1.da,da) == true)
+			diem+=2;	
+		dscau.push_back(bd1);
 	}
+	cout << "diem: " << diem << endl;
+	fflush(stdin);
+	cout << "nhap ten: ";
+	fflush(stdin);
+	getline(cin,ten);
+	luudiem(ten,diem);
 }
 
 
