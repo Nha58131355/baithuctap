17e4a35d711bbf01d38b2a2b722530a22a48b80c
Nha58131355 <ctvngsky@gmail.com>
2018-12-28 09:40:58 +0700

    Update bai4.cpp

--- bai4.cpp
+++ bai4.cpp
@@ -6,31 +6,57 @@ using namespace std;
 fstream fi;
 
 
-void DocFile(int a[10][10],int n)
+void DocFile(int a[100][100],int n)
 {
 	for(int i = 0; i < n; i++)
-		for(int j=0;j < n;j++)
+	{
+		for(int j=0;j < n;j++){
+			
 			fi >> a[i][j];
-	fi.close();
+		}
+	}	
+	
 }
 
-void XuatDoThi(int a[10][10],int n)
+void XuatDoThi(int a[100][100],int n)
+{
+	for(int i = 0; i < n; i++)
 	{
-		for(int i = 0; i < n; i++)
-		{
 		for(int j=0;j < n;j++)
-		
 			cout << a[i][j]<<"  ";
-			cout<<endl;
-		}
+		cout<<endl;
 	}
+		
+}
 
+void TaoMoi(int a[100][100], int &n)
+{
+	cout << "nhap n: ";
+	cin >> n;
+	fi.open("timduong.txt", ios::out);
+	fi << n <<endl;
+	for(int i=0; i < n; i++)
+	{
+		for(int j=0; j < n; j++)
+		{
+			cout << "trong so "  << i << " den" << j<<": ";
+			cin >> a[i][j];
+			fi << a[i][j] <<" ";	
+		}
+		cout<<endl;
+		fi << endl;	
+	}
+	
+	fi.close();
+}
 int main()
 {
 	int n;
+	int a[100][100];
+	TaoMoi(a,n);
 	fi.open("timduong.txt",ios::in);
 	fi>>n;
-	int a[10][10];
 	DocFile(a,n);
 	XuatDoThi(a,n);
+	
 }
